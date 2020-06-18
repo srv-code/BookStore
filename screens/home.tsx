c0bedc3ca5ed7a5f6df60d885a61052ff83de984
Sourav Dey <sourav.dey@hashedin.com>
2020-06-18 08:15:24 +0530

    Added Top Collections label in home screen

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Content } from 'native-base';
+import { Container, Content, Badge, Text } from 'native-base';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import GenreListCard from '../components/cards/genreListCard';
 import { globalStyles } from '../shared/styles/globalStyles';
@@ -13,12 +13,21 @@ export default function home(props: HomeProps) {
   };
 
   const genreList = getBookGenres();
-  
+
   return (
     <Container style={globalStyles.container}>
       <ScrollView>
         <Content padder>
-          <GenreListCard genreList={genreList} onGenreSelect={genreSelectHandler} />
+          <GenreListCard
+            genreList={genreList}
+            onGenreSelect={genreSelectHandler}
+          />
+
+          <View style={{ alignItems: 'center' }}>
+            <Text style={styles.textTopCollections}>Top Collections</Text>
+          </View>
+
+          
 
           {/* <BookCard /> */}
         </Content>
@@ -27,4 +36,26 @@ export default function home(props: HomeProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textTopCollections: {
+    backgroundColor: 'dodgerblue',
+    color: 'white',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    height: 50,
+    width: 300,
+    marginTop: 10,
+    marginBottom: 7,
+    borderRadius: 50,
+    fontSize: 30,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+});
--- shared/styles/globalStyles.ts
+++ shared/styles/globalStyles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DCDCDC',
     
     // alignItems: 'center',
     // justifyContent: 'center',
